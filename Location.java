78cb747f0d7cd67724143e67224ea53c350c309b
MaxwellHo <43505106+MaxwellHo@users.noreply.github.com>
2022-05-19 10:41:03 -0700

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,8 @@
+package textExcel;
+
+public interface Location
+{
+	// represents a location like B6, must be implemented by your SpreadsheetLocation class
+	int getRow(); // gets row of this location
+	int getCol(); // gets column of this location
+}
