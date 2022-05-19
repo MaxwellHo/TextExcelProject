73b1b073705a1d66e56527d7667b6d13ff471a09
MaxwellHo <43505106+MaxwellHo@users.noreply.github.com>
2022-05-19 10:40:10 -0700

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,7 @@
+package textExcel;
+
+public interface Cell
+{
+	public String abbreviatedCellText(); // text for spreadsheet cell display, must be exactly length 10
+	public String fullCellText(); // text for individual cell inspection, not truncated or padded
+}
